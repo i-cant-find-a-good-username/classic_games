eb35174d1e0603850505c1631eead240e49359c7
ilyes-guy <random_dude_233@proton.me>
2023-01-26 20:00:42 +0100

    start

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,23 @@
+<script>
+	import Nav_bar from './Nav_bar.svelte';
+	import Footer from './Footer.svelte';
+	import './app.css';
+</script>
+
+<div class="app">
+	<Nav_bar />
+
+	<main>
+		<slot />
+	</main>
+
+	<Footer />
+</div>
+
+<style>
+    .app{
+        
+    }
+
+
+</style>
---
+++ src/routes/Nav_bar.svelte
@@ -0,0 +1,39 @@
+<script>
+    
+</script>
+
+<div class="navbar">
+    <div>icon</div>
+    <div>
+        <button class='nav_buttons active_button' >play</button>
+        <button class='nav_buttons' >chill</button>
+    </div>
+    <div>
+        icon icon
+    </div>
+</div>
+
+<style>
+    .navbar{
+        padding: 10px;
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+    }
+    .nav_buttons{
+        padding-right: 15px;
+        padding-left: 15px;
+        padding-top: 5px;
+        padding-bottom: 5px;
+        border: 1px solid white;
+        background-color: transparent;
+        color: white;
+        border-radius: 10px;
+        font-size: 16px;
+        font-weight: 600;
+    }
+    .active_button{
+        color: aqua;
+        border-color: aqua;
+    }
+</style>
