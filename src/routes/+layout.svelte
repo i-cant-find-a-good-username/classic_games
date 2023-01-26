c3f14163e075177093df995f85b6de0668e50659
ilyes-guy <random_dude_233@proton.me>
2023-01-26 20:23:48 +0100

    2

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,13 +10,32 @@
 	<main>
 		<slot />
 	</main>
+    <div id='back_img'>
+         
+    </div>
 
 	<Footer />
 </div>
 
 <style>
     .app{
-        
+        background-color: transparent;
+    }
+    #back_img{
+        z-index: -100;
+        position: absolute;
+        top: 0;
+        left: 0;
+        height: 100%;
+        width: 100%;
+	    background-image: url('../lib/images/img.jpg');
+	    background-color: #cccccc;
+	    background-position: center;
+	    background-repeat: no-repeat;
+	    background-size: cover;
+	    filter: blur(8px);
+	    -webkit-filter: blur(8px);
+	
     }
 
 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,41 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+
+</script>
+
+
+<div>
+    <div id='title_texts'>
+        <h1>Play Games</h1>
+        <p>enjoy</p>
+    </div>
+
+    <div id='grid'>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
+</div>
+
+
+<style>
+    #title_texts{
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+    }
+    #grid{
+        display: grid;
+        
+        column-gap: 50px;       
+        row-gap: 50px;
+    }
+    #grid > *{
+        width: 100px;
+        height: 100px;
+        background-color: red;
+    }
+</style>
