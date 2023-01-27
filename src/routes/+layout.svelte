b3e7c8dc407d7502dcc570384a153b5d391ad982
ilyes-guy <random_dude_233@proton.me>
2023-01-27 11:37:49 +0100

    d

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,10 +2,20 @@
 	import Nav_bar from './Nav_bar.svelte';
 	import Footer from './Footer.svelte';
 	import './app.css';
+
+
+	const change_back = (e) => {
+		console.log( e.detail.pressed )
+		if(e.detail.pressed === 'play'){
+
+		}else if(e.detail.pressed === 'chill'){
+			
+		}
+	}
 </script>
 
 <div class="app">
-	<Nav_bar />
+	<Nav_bar on:button_changed={change_back} />
 
 	<main>
 		<slot />
@@ -24,13 +34,15 @@
 		overflow-y: scroll;
     }
     #back_img{
-		/*transform: rotate(10deg);*/
+		/*transform: rotate(10deg);
+		scale: 1.25;*/
+		transition: all 800ms cubic-bezier(.05,.43,.25,.95);
 		z-index: -100;
         position: absolute;
         top: 0;
         left: 0;
-        height: 100%;
-        width: 100%;
+        height: 100vh;
+        width: 100vw;
 	    background-image: url('../lib/images/img.jpg');
 	    background-color: #cccccc;
 	    background-position: center;
--- src/routes/Nav_bar.svelte
+++ src/routes/Nav_bar.svelte
@@ -1,15 +1,26 @@
 <script>
     
+    import volume from '$lib/images/volume.svg';
+    import moon from '$lib/images/moon.svg';
+
+    import { createEventDispatcher } from "svelte"
+    const dispatch = createEventDispatcher()
+    const test = (e) => {
+        dispatch('button_changed', {
+            pressed: e.target.innerText
+        })
+    }
 </script>
 
 <div class="navbar">
     <div>icon</div>
     <div>
-        <button class='nav_buttons active_button' >play</button>
-        <button class='nav_buttons' >chill</button>
+        <button class='nav_buttons active_button' on:click={test} >play</button>
+        <button class='nav_buttons' on:click={test} >chill</button>
     </div>
-    <div>
-        icon icon
+    <div id='right_icons'>
+        <img src={volume} alt="Welcome" />
+        <img src={moon} alt="Welcome" />
     </div>
 </div>
 
@@ -32,9 +43,35 @@
         font-size: 16px;
         font-weight: 600;
         cursor: pointer;
+		transition: all 200ms cubic-bezier(.05,.43,.25,.95);
+    }
+    .nav_buttons:not(.active_button):hover{
+        background-color: rgb(200, 200, 200);
+        color: black;
+    }
+    .active_button:hover{
+        background-color: aqua;
+        color: rgb(200, 200, 200);
     }
     .active_button{
         color: aqua;
         border-color: aqua;
     }
+    #right_icons{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+    #right_icons > *{
+        padding: 10px;
+        border-radius: 100%;
+        cursor: pointer;
+        border: 2px solid transparent;
+		transition: all 200ms cubic-bezier(.05,.43,.25,.95);
+    }
+    #right_icons > *:hover{
+        background-color: rgba(200,200,200,0.2);
+        border-color: rgb(200, 200, 200);
+    }
+
 </style>
