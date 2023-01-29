147b5149af76ec42add8078275f3de9775b27533
ilyes-guy <random_dude_233@proton.me>
2023-01-29 18:23:49 +0100

    layouts

---
+++ src/routes/sudoko/+page.svelte
@@ -0,0 +1,156 @@
+<script>
+
+</script>
+
+<div id='main_parent'>
+    <div id='main'>
+        <div id='main_grid'>
+            <div class='sub_grid'>
+                <div class='cell'></div>
+                <div class='cell'></div>
+                <div class='cell'></div>
+                <div class='cell'></div>
+                <div class='cell'></div>
+                <div class='cell'></div>
+                <div class='cell'></div>
+                <div class='cell'></div>
+                <div class='cell'></div>
+            </div>
+            <div class='sub_grid'>
+                <div class='cell'></div>
+                <div class='cell'></div>
+                <div class='cell'></div>
+                <div class='cell'></div>
+                <div class='cell'></div>
+                <div class='cell'></div>
+                <div class='cell'></div>
+                <div class='cell'></div>
+                <div class='cell'></div>
+            </div>
+            <div class='sub_grid'>
+                <div class='cell'></div>
+                <div class='cell'></div>
+                <div class='cell'></div>
+                <div class='cell'></div>
+                <div class='cell'></div>
+                <div class='cell'></div>
+                <div class='cell'></div>
+                <div class='cell'></div>
+                <div class='cell'></div>
+            </div>
+            <div class='sub_grid'>
+                <div class='cell'></div>
+                <div class='cell'></div>
+                <div class='cell'></div>
+                <div class='cell'></div>
+                <div class='cell'></div>
+                <div class='cell'></div>
+                <div class='cell'></div>
+                <div class='cell'></div>
+                <div class='cell'></div>
+            </div>
+            <div class='sub_grid'>
+                <div class='cell'></div>
+                <div class='cell'></div>
+                <div class='cell'></div>
+                <div class='cell'></div>
+                <div class='cell'></div>
+                <div class='cell'></div>
+                <div class='cell'></div>
+                <div class='cell'></div>
+                <div class='cell'></div>
+            </div>
+            <div class='sub_grid'>
+                <div class='cell'></div>
+                <div class='cell'></div>
+                <div class='cell'></div>
+                <div class='cell'></div>
+                <div class='cell'></div>
+                <div class='cell'></div>
+                <div class='cell'></div>
+                <div class='cell'></div>
+                <div class='cell'></div>
+            </div>
+            <div class='sub_grid'>
+                <div class='cell'></div>
+                <div class='cell'></div>
+                <div class='cell'></div>
+                <div class='cell'></div>
+                <div class='cell'></div>
+                <div class='cell'></div>
+                <div class='cell'></div>
+                <div class='cell'></div>
+                <div class='cell'></div>
+            </div>
+            <div class='sub_grid'>
+                <div class='cell'></div>
+                <div class='cell'></div>
+                <div class='cell'></div>
+                <div class='cell'></div>
+                <div class='cell'></div>
+                <div class='cell'></div>
+                <div class='cell'></div>
+                <div class='cell'></div>
+                <div class='cell'></div>
+            </div>
+            <div class='sub_grid'>
+                <div class='cell'></div>
+                <div class='cell'></div>
+                <div class='cell'></div>
+                <div class='cell'></div>
+                <div class='cell'></div>
+                <div class='cell'></div>
+                <div class='cell'></div>
+                <div class='cell'></div>
+                <div class='cell'></div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<style>
+    #main_parent{
+        padding: 50px;
+    }
+    #main{
+        box-sizing: border-box;
+        width: 500px;
+        aspect-ratio: 1/1;
+        margin: auto;
+        border: 4px solid #1f242d;
+        background-color: #1f242d;
+    }
+
+    #main_grid{
+        width: 100%;
+        height: 100%;
+        display: grid;
+        grid-template-columns: repeat(3, minmax(0, 1fr));
+        column-gap: 5px;       
+        row-gap: 5px;
+    }
+    .sub_grid{
+        width: 100%;
+        height: 100%;
+        display: grid;
+        grid-template-columns: repeat(3, minmax(0, 1fr));
+        column-gap: 2px;       
+        row-gap: 2px;
+    }
+    .cell{
+        width: 100%;
+        height: 100%;
+        background-color: #2a303c;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+    /*test*/
+    .sub_grid > :first-child{
+        background-color: #4b5563;
+    }
+    .sub_grid > :nth-child(2){
+        background-color: #eb5757ff;
+    }
+
+</style>
---
+++ src/routes/tile_slide/+page.svelte
@@ -0,0 +1,45 @@
+<script>
+
+</script>
+
+<div id='main_parent'>
+    <div id='main'>
+        <div class='cell'>5</div>
+        <div class='cell'>5</div>
+        <div class='cell'>5</div>
+        <div class='cell'>5</div>
+        <div class='cell'>5</div>
+        <div class='cell'>5</div>
+        <div class='cell'>5</div>
+        <div class='cell'>1</div>
+    </div>
+</div>
+
+<style>
+    #main_parent{
+        padding: 50px;
+    }
+    #main{
+        box-sizing: border-box;
+        width: 450px;
+        aspect-ratio: 1/1;
+        margin: auto;
+        border: 4px solid #1f242d;
+        background-color: #1f242d;
+        display: grid;
+        grid-template-columns: repeat(3, minmax(0, 1fr));
+        column-gap: 5px;       
+        row-gap: 5px;
+    }
+    .cell{
+        background-color: #a6adbaff;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        font-size: 50px;
+        font-weight: 600;
+        color: #1f242d;
+    }
+
+
+</style>
