b376d0eaa3a48b384d30351531750c0f32338ae3
ilyes-guy <random_dude_233@proton.me>
2023-02-21 19:36:17 +0100

    icons

--- src/routes/tic_tac_toe/+page.svelte
+++ src/routes/tic_tac_toe/+page.svelte
@@ -1,24 +1,73 @@
 <script>
+    import circle from '$lib/images/circle.svg';
+    import x from '$lib/images/x.svg';
 
 
+
+    let cell_1 = false
+    let cell_2 = false
+    let cell_3 = false
+    let cell_4 = false
+    let cell_5 = false
+    let cell_6 = false
+    let cell_7 = false
+    let cell_8 = false
+    let cell_9 = false
 
 </script>
 
 <div id='main_parent'>
     <div id='main'>
         <div class="cell">
-
+            <svg version="1.1" id="circle" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+            viewBox="0 0 100 100" xml:space="preserve">
+              <circle fill="none" stroke="#70d6ff" stroke-width="10" cx="50" cy="50" r="48" stroke-dasharray="360" stroke-linecap="round" transform="rotate(-90 ) translate(-100 0)" >
+                  <animate attributeName="stroke-dashoffset" values="360;0" dur="2s" repeatCount="nonet"></animate>
+              </circle>
+          </svg>
             
             
         </div>
-        <div class="cell"></div>
-        <div class="cell"></div>
-        <div class="cell"></div>
-        <div class="cell"></div>
-        <div class="cell">d</div>
-        <div class="cell">d</div>
-        <div class="cell">d</div>
-        <div class="cell">d</div>
+        <div class="cell" on:click={() => {cell_1 = true }} >
+            {#if cell_1}
+                <img class='player_move' src={circle} />
+            {/if}
+        </div>
+        <div class="cell" on:click={() => {cell_2 = true }} >
+            {#if cell_2}
+                <img class='player_move' src={x} />
+            {/if}
+        </div>
+        <div class="cell" on:click={() => {cell_3 = true }} >
+            {#if cell_3}
+                hello
+            {/if}
+        </div>
+        <div class="cell" on:click={() => {cell_4 = true }} >
+            {#if cell_4}
+                hello
+            {/if}
+        </div>
+        <div class="cell" on:click={() => {cell_5 = true }} >
+            {#if cell_5}
+                hello
+            {/if}
+        </div>
+        <div class="cell" on:click={() => {cell_6 = true }} >
+            {#if cell_6}
+                hello
+            {/if}
+        </div>
+        <div class="cell" on:click={() => {cell_7 = true }} >
+            {#if cell_7}
+                hello
+            {/if}
+        </div>
+        <div class="cell" on:click={() => {cell_8 = true }} >
+            {#if cell_8}
+                hello
+            {/if}
+        </div>
     </div>
     
 </div>
@@ -39,6 +88,8 @@
     }
 
    .cell{
+        padding: 25px;
+        box-sizing: border-box;
         width: 100%;
         height: 100%;
         aspect-ratio: 1/1;
@@ -78,5 +129,13 @@
     #main > :nth-child(6),
     #main > :nth-child(9){
         border-right: 2px solid transparent;
+    }
+
+
+
+
+    .player_move{
+        width: 100%;
+        height: 100%;
     }
 </style>
