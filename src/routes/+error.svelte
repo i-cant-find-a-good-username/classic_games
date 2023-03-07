a5bbaa1543087986de54495ea08093266f473128
ilyes-guy <random_dude_233@proton.me>
2023-03-07 23:50:15 +0100

    error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,35 @@
+<div id='main'>
+
+    <div>
+        <h1>the error</h1>
+        <a href="/">HOME</a>
+    </div>
+</div>
+
+<style>
+
+    #main{
+        height: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+    #main > :first-child{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+    }
+
+    a{
+        background-color: #1f242d;
+        color: white;
+        text-decoration: none;
+        padding-right: 20px;
+        padding-left: 20px;
+        padding-top: 10px;
+        padding-bottom: 10px;
+        border-radius: 10px;
+    }
+
+</style>
