6b9e744994f4e18f726c8a1c7af793aec39c3c02
noot-noot-pengu <random_dude_233@proton.me>
2023-07-15 15:14:50 +0100

    style

--- src/routes/sudoko/+page.svelte
+++ src/routes/sudoko/+page.svelte
@@ -46,6 +46,18 @@
 			{/each}
 		</div>
 	</div>
+	<div id='controls'>
+		<button>1</button>
+		<button>2</button>
+		<button>3</button>
+		<button>4</button>
+		<button>5</button>
+		<button>6</button>
+		<button>7</button>
+		<button>8</button>
+		<button>9</button>
+		<button>X</button>
+	</div>
 </div>
 
 <style>
@@ -54,16 +66,43 @@
 		display: flex;
 		align-items: center;
 		justify-content: center;
+		flex-direction: column;
 	}
 	#main{
 		box-sizing: border-box;
 		width: 500px;
 		aspect-ratio: 1/1;
-		margin: auto;
+		/*margin: auto;*/
 		border: 4px solid #1f242d;
 		background-color: #1f242d;
 	}
-
+	#controls{
+		padding-top: 10px;
+		width: 500px;
+		display: grid;
+		grid-template-columns: repeat(5, minmax(0, 1fr));
+		gap: 2px;
+	}
+	#controls > * {
+		font-size: large;
+		padding: 10px;
+		width: 100%;
+		border: none;
+		background-color: #1f242d;
+		color: #a6adbaff;
+	}
+	#controls > :nth-child(1) {
+		border-radius: 10px 0px 0px 0px;
+	}
+	#controls > :nth-child(5) {
+		border-radius: 0px 10px 0px 0px;
+	}
+	#controls > :nth-child(6) {
+		border-radius: 0px 0px 0px 10px;
+	}
+	#controls > :nth-child(10) {
+		border-radius: 0px 0px 10px 0px;
+	}
 	#main_grid{
 		width: 100%;
 		height: 100%;
@@ -81,6 +120,7 @@
 		row-gap: 2px;
 	}
 	.cell{
+		cursor: pointer;
 		width: 100%;
 		height: 100%;
 		background-color: #2a303c;
